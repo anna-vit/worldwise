3580826574fcbe92646bd6537bf8bfbe4062dbbe
anna-vit <sidorovahanna795@gmail.com>
2024-02-01 11:49:47 -0500

    Added Face Authentication context, login, protecting a route

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,16 +1,33 @@
 import PageNav from "../components/PageNav";
 import styles from "./Login.module.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useAuth } from "../contexts/FakeAuthContext";
+import Button from "../components/Button";
+import { useNavigate } from "react-router-dom";
 
 export default function Login() {
   // PRE-FILL FOR DEV PURPOSES
   const [email, setEmail] = useState("jack@example.com");
   const [password, setPassword] = useState("qwerty");
+  const { login, isAuthenticated } = useAuth();
+  const navigation = useNavigate();
 
+  function handelSubmit(event) {
+    event.preventDefault();
+    if (email && password) login(email, password);
+    console.log("handelUserSubmit");
+  }
+
+  useEffect(
+    function () {
+      if (isAuthenticated) navigation("/app", { replace: true });
+    },
+    [isAuthenticated, navigation]
+  );
   return (
     <main className={styles.login}>
       <PageNav />
-      <form className={styles.form}>
+      <form className={styles.form} onSubmit={handelSubmit}>
         <div className={styles.row}>
           <label htmlFor="email">Email address</label>
           <input
@@ -32,7 +49,9 @@ export default function Login() {
         </div>
 
         <div>
-          <button>Login</button>
+          <Button type="primary" onClick={handelSubmit}>
+            Login
+          </Button>
         </div>
       </form>
     </main>
